caaab9c742afae6de308797882d9ac17ea1b22be
Lfifolato <ldalri1999@gmail.com>
2020-07-01 22:10:37 -0300

    Atualização para statefull

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 
 main() => runApp(new Perguntados());
 
-class Perguntados extends StatelessWidget {
+class perguntaApp extends State<Perguntados> {
+  var perguntaSelecionada = 0;
+
   void respostas() {
+    setState(() {
+      perguntaSelecionada++;
+    });
     print("Sua Resposta");
   }
 
-  final perguntas = [
-    'Qual seu animal favorito  ?',
-    'Qual e sua cor favorita ? '
-        'Qual e seu Signo favorito ?'
-  ];
-
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual seu animal favorito  ?',
+      'Qual e sua cor favorita ? ',
+      'Qual e seu Signo favorito ?',
+    ];
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -23,22 +28,29 @@ class Perguntados extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas.elementAt(0)),
+            Text(perguntas[perguntaSelecionada]),
             RaisedButton(
               child: Text("Pergunta 1"),
-              onPressed: () => this.respostas(),
+              onPressed: respostas,
             ),
             RaisedButton(
               child: Text("Pergunta 1"),
-              onPressed: () => this.respostas(),
+              onPressed: respostas,
             ),
             RaisedButton(
               child: Text("Pergunta 1"),
-              onPressed: () => this.respostas(),
+              onPressed: respostas,
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class Perguntados extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return perguntaApp();
   }
 }
